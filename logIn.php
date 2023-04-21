40c0f12547e242ce204d44fd85904e1a4348ecca
AndrewDority <adority121@gmail.com>
2023-04-20 20:33:01 -0500

    removed internal css, php db connection successful

--- logIn.php
+++ logIn.php
@@ -1,9 +1,9 @@
 <?php
-    $conn = mysql_connect("mysql.eecs.ku.edu", "a815d085", "ohni7oklhJ") or die('could not connect' . mysql_error());
+    $conn = mysql_connect("mysql.eecs.ku.edu", "a815d085", "ohni7ohJ") or die('could not connect' . mysql_error());
 	mysql_select_db('a815d085') or die('Could not select database');
     $q = mysql_query('SELECT * FROM CRUISE') or die('My SQL query failed');
     echo "<table>\n";
-	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	while ($line = mysql_fetch_array($q, MYSQL_ASSOC)) {
     		echo "\t<tr>\n";
     		foreach ($line as $col_value) {
         		echo "\t\t<td>$col_value</td>\n";
@@ -12,6 +12,8 @@
 	}
 	echo "</table>\n";
 
-	echo "Number of fields: ".mysql_num_fields($result)."<br>";
-	echo "Number of records: ".mysql_num_rows($result)."<br>";
+	echo "Number of fields: ".mysql_num_fields($q)."<br>";
+	echo "Number of records: ".mysql_num_rows($q)."<br>";
+	mysql_free_result($q);
+	mysql_close($link);
 ?>
