ca9cc37ea3266a318024d04e14dbb78b2472630d
TristanTjandra <t365t737@home.ku.edu>
2023-04-25 18:47:59 -0500

    committed

--- searchBook.php
+++ searchBook.php
@@ -1,8 +1,31 @@
 <?php
-    $conn = mysqli_connect('mysql.eecs.ku.edu', 't365t737', 'aiPho4UN', 't365t737');
 
     echo '<p style = "text-align: center;">';
+    
 
     //check if search bar is empty
-    if (empty($_POST['']))
+    if (empty($_POST['search'])){
+        echo 'No results.</p>';
+        die()
+    }
+    $conn = mysqli_connect('mysql.eecs.ku.edu', 't365t737', 'aiPho4UN', 't365t737');
+    elseif ($_POST['Option'] == 'title'){
+        $input = $_POST['search'];
+        $query = "select * from Book where title = '$input'";
+        $q_result = mysqli_query($conn, $query);
+        echo '<table>';
+        echo '<tr>
+        <th>Author</th>
+        <th>ISBN</th>
+        <th>Title</th>
+        </tr>';
+        while($line = mysqli_fetch_array($q_result, MYSQLI_ASSOC)){
+            echo '<tr>' . 
+            '<td>' . $line['author'] . '</td>' .
+            '<td>' . $line['ISBN'] . '</td>' .
+            '<td>' . $line['title'] . '</td>' .
+            '</tr>';
+        }
+        echo '</table>';
+    }
 ?>
