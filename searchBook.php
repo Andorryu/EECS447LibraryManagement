9db754ed352794f70753ce215cf9230a57ee5b46
TristanTjandra <t365t737@home.ku.edu>
2023-04-25 18:08:19 -0500

    Added searchBook.php and modified search.html

---
+++ searchBook.php
@@ -0,0 +1,8 @@
+<?php
+    $conn = mysqli_connect('mysql.eecs.ku.edu', 't365t737', 'aiPho4UN', 't365t737');
+
+    echo '<p style = "text-align: center;">';
+
+    //check if search bar is empty
+    if (empty($_POST['']))
+?>
