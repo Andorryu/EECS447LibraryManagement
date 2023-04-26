b7e3afeba3ad1bfaa16c26eeec031c887d345cd7
andorryu <adority121@gmail.com>
2023-04-25 19:04:42 -0500

    search for patron

--- searchPatron.php
+++ searchPatron.php
@@ -15,8 +15,28 @@
         echo 'Please select a term to search by.</p>';
         die();
     }
+    echo '</p>';
 
+    $search = $_POST['search'];
+    $option = $_POST['option'];
     $conn = mysqli_connect('localhost', 'root', 'password', 'LMS');
-    $query = "select * from Patron where";
+    $query = "select * from Patron where $option='$search'";
+    $q_result = mysqli_query($conn, $query);
 
+    echo '<table style="border-style: solid;">';
+    echo '<tr>
+    <th>PID</th>
+    <th>Last Name</th>
+    <th>First Name</th>
+    <th>Phone #</th>
+    </tr>';
+    while ($line = mysqli_fetch_array($q_result, MYSQLI_ASSOC)) {
+        echo '<tr>' .
+        '<td>' . $line['PID'] . '</td>' .
+        '<td>' . $line['lastName'] . '</td>' .
+        '<td>' . $line['firstName'] . '</td>' .
+        '<td>' . $line['phone'] . '</td>' .
+        '</tr>';
+    }
+    echo "</table>";
 ?>
