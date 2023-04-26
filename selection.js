7204a0a9744f5a24dc1a38065b407e4446bc245c
andorryu <adority121@gmail.com>
2023-04-25 20:24:46 -0500

    sel

--- selection.js
+++ selection.js
@@ -2,4 +2,6 @@
 function selectPatron(PID) {
     // add PID of selected patron to div for formatting and
     // add PID as form value
+    selection = document.querySelector('.selectedPatron');
+    selection.innerText('test');
 }
