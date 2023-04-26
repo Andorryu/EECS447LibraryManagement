897093d4ba4f8cc065246e8d94e11093c1f15409
TristanTjandra <t365t737@home.ku.edu>
2023-04-25 19:06:41 -0500

    fixed searchBook

--- searchBook.php
+++ searchBook.php
@@ -6,28 +6,28 @@
     //check if search bar is empty
     if (empty($_POST['search'])){
         echo 'No results.</p>';
-        die()
+        die();
     }
+    
     $conn = mysqli_connect('mysql.eecs.ku.edu', 't365t737', 'aiPho4UN', 't365t737');
-    elseif (){
-        $input = $_POST['search'];
-        $table = $_POST['Option'];
-        $query = "select * from Book where $table = '$input'";
-        $q_result = mysqli_query($conn, $query);
-        echo '<table>';
-        echo '<tr>
-        <th>Author</th>
-        <th>ISBN</th>
-        <th>Title</th>
-        </tr>';
-        while($line = mysqli_fetch_array($q_result, MYSQLI_ASSOC)){
-            echo '<tr>' . 
-            '<td>' . $line['author'] . '</td>' .
-            '<td>' . $line['ISBN'] . '</td>' .
-            '<td>' . $line['title'] . '</td>' .
-            '</tr>';
-        }
-        echo '</table>';
+    $input = $_POST['search'];
+    $table = $_POST['Option'];
+    $query = "select * from Book where $table = '$input'";
+    $q_result = mysqli_query($conn, $query);
+    echo '<table>';
+    echo '<tr>
+    <th>Author</th>
+    <th>ISBN</th>
+    <th>Title</th>
+    </tr>';
+    while($line = mysqli_fetch_array($q_result, MYSQLI_ASSOC)){
+        echo '<tr>' . 
+        '<td>' . $line['author'] . '</td>' .
+        '<td>' . $line['ISBN'] . '</td>' .
+        '<td>' . $line['title'] . '</td>' .
+        '</tr>';
     }
+    echo '</table>';
+
 
 ?>
