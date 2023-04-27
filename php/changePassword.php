3c700fcbc65529acc81a7eef4bb7b811312c2770
andorryu <adority121@gmail.com>
2023-04-26 19:22:18 -0500

    password change

--- php/changePassword.php
+++ php/changePassword.php
@@ -1,5 +1,8 @@
 <?php
     include 'creds.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     echo '<p style="color: red; text-align: center;">';
 
     // email
@@ -23,23 +26,34 @@
         echo 'Your current password and new password cannot be the same.</p>';
         die();
     }
-    elseif ($newPass < 8) {
+    elseif (strlen($newPass) < 8) {
         echo 'Your new password must be 8 or more characters long.</p>';
         die();
     }
-    elseif (preg_match('/[a-z]/', $newPass)) {
+    elseif (preg_match('/[a-z]/', $newPass) == 0) {
         echo 'Your new password must contain one or more lowercase letters.</p>';
         die();
     }
-    elseif (preg_match('/[A-Z]/', $newPass)) {
+    elseif (preg_match('/[A-Z]/', $newPass) == 0) {
         echo 'Your new password must contain one or more uppercase letters.</p>';
         die();
     }
-    elseif (preg_match('/[0-9]/', $newPass)) {
+    elseif (preg_match('/[0-9]/', $newPass) == 0) {
         echo 'Your new password must contain one or more numbers.</p>';
         die();
     }
 
+    $email = $_POST['email'];
     $conn = mysqli_connect($server_, $username_, $password_, $database_);
-    $query = '';
+    $query = "select * from Librarian where email='$email' and password='$oldPass';";
+    $result = mysqli_fetch_array(mysqli_query($conn, $query));
+    if ($result == null) {
+        echo "$result";
+        echo 'Email and/or password is incorrect.</p>';
+        die();
+    }
+
+    $query = "update Librarian set password='$newPass' where email='$email';";
+    mysqli_query($conn, $query);
+    echo '</p><p style="color: green; text-align: center;">Password changed.</p>';
 ?>
